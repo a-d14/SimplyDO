e4c6933d9d0a8eaa6fbbce96e1cb3f483aca6b82
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-06-26 17:19:05 -0500

    Added SVG Icons and made changes to UI

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -887,53 +887,6 @@ class ItemView extends (0, _viewJsDefault.default) {
     constructor(){
         super();
     }
-    _generateMarkup() {
-        if (this._data.edit) {
-            console.log(this._data.content);
-            return `
-                <div class="list-item">
-                    <li data-id=${this._data.id}><input type="text" value="${this._data.content}"/></li>
-                    <div class="list-item__controls">
-                        <button type="button" class="list-item__controls--delete">
-                            <svg class="delete">
-                                <use href="${0, _iconsSvgDefault.default}#icon-bin"></use>
-                            </svg>
-                        </button>
-                        <button type="button" class="list-item__controls--submit">
-                            <svg class="submit">
-                                <use href="${0, _iconsSvgDefault.default}#icon-plus"></use>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            `;
-        } else return `
-                <div class="list-item">
-                    <li data-id=${this._data.id}>${this._data.completed ? `<s>${this._data.content}</s>` : `${this._data.content}`}</li>
-                    <div class="list-item__controls">
-                        <button type="button" class="list-item__controls--complete">
-                            ${!this._data.completed ? `<svg class="check">
-                                    <use href="${0, _iconsSvgDefault.default}#icon-checkmark2"></use>
-                                </svg>` : `
-                                <svg class="cross">
-                                    <use href="${0, _iconsSvgDefault.default}#icon-cross"></use>
-                                </svg>
-                                `}
-                        </button>
-                        <button type="button" class="list-item__controls--edit">
-                            <svg class="edit">
-                                <use href="${0, _iconsSvgDefault.default}#icon-pencil"></use>
-                            </svg>
-                        </button>
-                        <button type="button" class="list-item__controls--delete">
-                            <svg class="delete">
-                                <use href="${0, _iconsSvgDefault.default}#icon-bin"></use>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            `;
-    }
 }
 exports.default = new ItemView();
 
--- src/js/views/ItemView.js
+++ src/js/views/ItemView.js
@@ -8,58 +8,58 @@ class ItemView extends View {
     }
     
 
-    _generateMarkup() {
-        if(this._data.edit) {
-            console.log(this._data.content);
-            return `
-                <div class="list-item">
-                    <li data-id=${this._data.id}><input type="text" value="${this._data.content}"/></li>
-                    <div class="list-item__controls">
-                        <button type="button" class="list-item__controls--delete">
-                            <svg class="delete">
-                                <use href="${icons}#icon-bin"></use>
-                            </svg>
-                        </button>
-                        <button type="button" class="list-item__controls--submit">
-                            <svg class="submit">
-                                <use href="${icons}#icon-plus"></use>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            `;
-        } else {
-            return `
-                <div class="list-item">
-                    <li data-id=${this._data.id}>${this._data.completed ? `<s>${this._data.content}</s>` : `${this._data.content}`}</li>
-                    <div class="list-item__controls">
-                        <button type="button" class="list-item__controls--complete">
-                            ${!this._data.completed ? 
-                                `<svg class="check">
-                                    <use href="${icons}#icon-checkmark2"></use>
-                                </svg>` : 
-                                `
-                                <svg class="cross">
-                                    <use href="${icons}#icon-cross"></use>
-                                </svg>
-                                `
-                            }
-                        </button>
-                        <button type="button" class="list-item__controls--edit">
-                            <svg class="edit">
-                                <use href="${icons}#icon-pencil"></use>
-                            </svg>
-                        </button>
-                        <button type="button" class="list-item__controls--delete">
-                            <svg class="delete">
-                                <use href="${icons}#icon-bin"></use>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            `;
-        }
-    }
+    // _generateMarkup() {
+    //     if(this._data.edit) {
+    //         console.log(this._data.content);
+    //         return `
+    //             <div class="list-item">
+    //                 <li data-id=${this._data.id}><input type="text" value="${this._data.content}"/></li>
+    //                 <div class="list-item__controls">
+    //                     <button type="button" class="list-item__controls--delete">
+    //                         <svg class="delete">
+    //                             <use href="${icons}#icon-bin"></use>
+    //                         </svg>
+    //                     </button>
+    //                     <button type="button" class="list-item__controls--submit">
+    //                         <svg class="submit">
+    //                             <use href="${icons}#icon-plus"></use>
+    //                         </svg>
+    //                     </button>
+    //                 </div>
+    //             </div>
+    //         `;
+    //     } else {
+    //         return `
+    //             <div class="list-item">
+    //                 <li data-id=${this._data.id}>${this._data.completed ? `<s>${this._data.content}</s>` : `${this._data.content}`}</li>
+    //                 <div class="list-item__controls">
+    //                     <button type="button" class="list-item__controls--complete">
+    //                         ${!this._data.completed ? 
+    //                             `<svg class="check">
+    //                                 <use href="${icons}#icon-checkmark2"></use>
+    //                             </svg>` : 
+    //                             `
+    //                             <svg class="cross">
+    //                                 <use href="${icons}#icon-cross"></use>
+    //                             </svg>
+    //                             `
+    //                         }
+    //                     </button>
+    //                     <button type="button" class="list-item__controls--edit">
+    //                         <svg class="edit">
+    //                             <use href="${icons}#icon-pencil"></use>
+    //                         </svg>
+    //                     </button>
+    //                     <button type="button" class="list-item__controls--delete">
+    //                         <svg class="delete">
+    //                             <use href="${icons}#icon-bin"></use>
+    //                         </svg>
+    //                     </button>
+    //                 </div>
+    //             </div>
+    //         `;
+    //     }
+    // }
 }
 
 export default new ItemView();
