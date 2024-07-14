e9ebef52ae335c5393dc579ba930871518c9d579
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-07-13 19:10:13 -0500

    Modified Add Item Form

--- src/js/views/addItemView.js
+++ src/js/views/addItemView.js
@@ -10,18 +10,18 @@ class AddItemView extends View {
                 <input class="main__list--add-item-button" type="date" />
                 <!-- <input class="main__list--add-item-button" type="datetime-local" /> -->
                 <input class="main__list--add-item-button" type="time" />
-                <button class="main__list--add-item-button">
-                    Category
+                <select class="main__list--add-item-button">
+                    <option value=default selected>Category</option>
                     <svg class="w-6 h-6 text-gray-800 dark:text-white icon--medium" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
                     </svg>  
-                </button>
-                <button class="main__list--add-item-button">
-                    Priority
+                </select>
+                <select class="main__list--add-item-button">
+                    <option value=default selected>Priority</option>
                     <svg class="w-6 h-6 text-gray-800 dark:text-white icon--medium" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
                     </svg>  
-                </button>
+                </select>
                 <button type="submit" class="main__list--add-item-button main__list--add-item-button-add">
                     <svg class="w-6 h-6 text-gray-800 dark:text-white icon icon--medium icon--plus" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
